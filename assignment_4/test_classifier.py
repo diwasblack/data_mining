3a26bf24a0a2bcea9a37e680ef5fdf5b61b6888a
Diwas Sharma <diwasblack@gmail.com>
2018-04-11 11:01:01 -0500

    Using holdout data from testing classifier accuracy

--- assignment_4/test_classifier.py
+++ assignment_4/test_classifier.py
@@ -4,6 +4,7 @@ import numpy as np
 import cloudpickle as pickle
 
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 from logistic_regression import LogisticRegression
 
@@ -14,12 +15,14 @@ logger = logging.getLogger(__name__)
 def main():
     mnist23 = pickle.load(open("mnist23.data", "rb"))
 
-    classifier = LogisticRegression()
-    y_train = np.array([1 if x == 3 else 0 for x in mnist23.target])
-    classifier.fit(mnist23.data, y_train)
+    x = mnist23.data
+    y = np.array([1 if x == 3 else 0 for x in mnist23.target])
 
-    x_test = mnist23.data
-    y_test = y_train
+    classifier = LogisticRegression()
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33)
+
+    classifier.fit(x_train, y_train)
 
     y_predicted = classifier.predict(x_test)
 
